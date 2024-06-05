554392acf4ca5112f33c1005ce27ded9def4ddf3
SAMEER RAM <sameerram9321@gmail.com>
2024-06-05 13:44:28 +0530

    Updated Header.jsx

--- Task_1-Landing_Page/src/Components/Header.jsx
+++ Task_1-Landing_Page/src/Components/Header.jsx
@@ -9,14 +9,14 @@ const Header = () => {
         <button className="bg-gradient-to-r from-blue-400 to-purple-400 font-medium text-sm text-[#2c2c2b] rounded-3xl px-2.5 py-1.5 lg:px-5 md:py-1">Real Madrid</button>
       </div>
       <ul className="hidden lg:flex justify-between items-center font-lato text-gray-400 gap-8 ">
-        <li><a className="text-blue-500 font-semibold text-[13px] px-4 hover:underline" href="#">Book Match Tickets</a></li>
-        <li><a className="text-blue-500 font-semibold text-[13px] px-3 hover:underline" href="#">Buy Jersey</a></li>
-        <li><a className="text-blue-500 font-semibold text-[13px] px-3 hover:underline" href="#">Santiago Bernabéu Stadium Ticekts</a></li>
-        <li><a className="text-blue-500 font-semibold text-[13px] px-3 hover:underline" href="#">Real Madrid's History</a></li>
+        <li><a className="text-blue-500 font-semibold text-[13px] px-4 hover:underline hover:font-extrabold" href="#">Book Match Tickets</a></li>
+        <li><a className="text-blue-500 font-semibold text-[13px] px-3 hover:underline hover:font-extrabold" href="#">Buy Jersey</a></li>
+        <li><a className="text-blue-500 font-semibold text-[13px] px-3 hover:underline hover:font-extrabold" href="#">Santiago Bernabéu Stadium Ticekts</a></li>
+        <li><a className="text-blue-500 font-semibold text-[13px] px-3 hover:underline hover:font-extrabold" href="#">Real Madrid's History</a></li>
       </ul>
       <div className="hidden lg:flex justify-center items-center font-lato gap-3">
-        <a className="text-blue-400 text-sm rounded-3xl bg-[#210563] px-5 py-1" href="#">Sign In</a>
-        <button className="text-blue-400 text-sm rounded-3xl bg-[#210563] px-5 py-1">Join Us</button>
+        <button className="text-blue-400 text-sm rounded-3xl bg-[#210563] px-5 py-1 hover:bg-blue-700 hover:text-black hover:font-bold" href="#">Sign In</button>
+        <button className="text-blue-400 text-sm rounded-3xl bg-[#210563] px-5 py-1 hover:bg-blue-700 hover:text-black hover:font-bold">Join Us</button>
       </div>
       <div className="lg:hidden">
         <FaBars />
